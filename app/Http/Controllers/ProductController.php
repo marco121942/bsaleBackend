9749137efbeea173019c0eb8b33db213f7a54b3d
codigo.enigma.front.marco <codigo.enigma.front.marco@gmail.com>
2022-11-12 20:31:06 -0500

    comment code

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,10 +14,11 @@ class ProductController extends Controller
 
     public function getProduct()
     {
+        #Obtenemos todos los productos de la tabla PRODUCT
         $product = DB::table('product')
             ->join('category', 'category.id', '=', 'product.category')->select('product.*', 'category.name as name_category')
             ->get();
-
+        #Modificamos el resultado según lo que el front necesite
         foreach ($product as $data) {
 
             if ($data->url_image == '') {
@@ -31,7 +32,7 @@ class ProductController extends Controller
             $data->price = "$" . $data->price;
         }
 
-
+        #Si no existe ningún error mandamos el resultado
         if ($product) {
             return response()->json([
                 'success' => true,
@@ -39,7 +40,7 @@ class ProductController extends Controller
                 'data' =>  $product
             ], 200);
         }
-
+        #Si  existe algún error mandamos un mensaje con un estado de error
         return response()->json([
             'success' => false,
             'message' => 'ocurrió algo inesperado',
@@ -49,11 +50,11 @@ class ProductController extends Controller
 
     public function getProductCategory()
     {
-
+        #Realizamos una consulta desde la relación  'products',ya que es el que une la tabla CATEGORY con la tabla PRODUCT y asi obtener los productos clasificados por su categoría
         $product = Category::with('products')->get();
 
 
-
+        #Modificamos el resultado según lo que el front necesite
         foreach ($product as $data) {
             foreach ($data->products as $item) {
                 if ($item->url_image == '') {
@@ -68,7 +69,7 @@ class ProductController extends Controller
             }
         }
 
-
+        #Si no existe ningún error mandamos el resultado
 
         if ($product) {
             return response()->json([
@@ -77,7 +78,7 @@ class ProductController extends Controller
                 'data' =>  $product
             ], 200);
         }
-
+        #Si  existe algún error mandamos un mensaje con un estado de error
         return response()->json([
             'success' => false,
             'message' => 'ocurrió algo inesperado',
@@ -86,8 +87,9 @@ class ProductController extends Controller
     }
     public function searchProduct(Request $request)
     {
+        #Ejecutamos la siguiente consulta, la cual tiene como finalidad comparar el nombre del producto con la palabra clave que es enviada por el FRONT
         $resultado = Product::where('name', 'like', '%' . $request->keyword . '%')->get();
-
+        #Modificamos el resultado según lo que el front necesite
         foreach ($resultado as $data) {
             if ($data->url_image == '') {
                 $data->url_image = "https://www.bicifan.uy/wp-content/uploads/2016/09/producto-sin-imagen.png";
@@ -99,6 +101,7 @@ class ProductController extends Controller
             }
             $data->price = "$" . $data->price;
         }
+        #Si no existe ningún error mandamos el resultado
         return response()->json([
             'success' => true,
             'message' => 'Resultado de la búsqueda',
@@ -108,8 +111,8 @@ class ProductController extends Controller
     public function filterProduct(Request $request)
     {
 
-     
 
+        //  start - Según las características que es enviada por el FRONT para el filtrado de productos,comparamos y ejecutamos el Query correcto para el filtrado
         $typeFilter = "";
         if ($request->typeFilter === 'category') {
             $typeFilter = "category";
@@ -122,11 +125,11 @@ class ProductController extends Controller
         if ($request->typeFilter === 'category') {
             $dataProduct = Product::where($typeFilter, $request->dataFilter)->join('category', 'category.id', '=', 'product.category')->select('product.*', 'category.name as name_category')->get();
         }
-        if($request->typeFilter === 'discount'){
+        if ($request->typeFilter === 'discount') {
             if ($request->dataFilter == 0) {
-                $dataProduct = Product::where('discount','=',0)->join('category', 'category.id', '=', 'product.category')->select('product.*', 'category.name as name_category')->get();
+                $dataProduct = Product::where('discount', '=', 0)->join('category', 'category.id', '=', 'product.category')->select('product.*', 'category.name as name_category')->get();
             } else if ($request->dataFilter == 1) {
-                $dataProduct = Product::where('discount','>',0)->join('category', 'category.id', '=', 'product.category')->select('product.*', 'category.name as name_category')->get();
+                $dataProduct = Product::where('discount', '>', 0)->join('category', 'category.id', '=', 'product.category')->select('product.*', 'category.name as name_category')->get();
             }
         }
         if ($request->typeFilter === 'imagen') {
@@ -136,7 +139,8 @@ class ProductController extends Controller
                 $dataProduct = Product::whereNotNull('url_image')->join('category', 'category.id', '=', 'product.category')->select('product.*', 'category.name as name_category')->get();
             }
         }
-
+        //  end - Según las características que es enviada por el FRONT para el filtrado de productos,comparamos y ejecutamos el Query correcto para el filtrado
+        #Modificamos el resultado según lo que el front necesite
         foreach ($dataProduct as $data) {
 
             if ($data->url_image == '') {
@@ -149,7 +153,7 @@ class ProductController extends Controller
             }
             $data->price = "$" . $data->price;
         }
-
+        #Si no existe ningún error mandamos el resultado
         return response()->json([
             'success' => true,
             'message' => 'Resultados del filtro',
--- routes/api.php
+++ routes/api.php
@@ -16,8 +16,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-
+#Servicio que tiene la finalidad de devvler todos los productos que existen en la BD
 Route::get('/v1/get/product', [ProductController::class, 'getProduct']);
+#Servicio encarga de devovler todos los productos clasificados según ala categoría ala que pertenece
 Route::get('/v1/get/productCategory', [ProductController::class, 'getProductCategory']);
+#Servicio encargado de devovler  los productos que coincidan con el nombre del producto,recive la palabra clave que es enviada por el FRONT 
 Route::post('/v1/search/product', [ProductController::class, 'searchProduct']);
+#Servicio encargado de devolver los productos filtrados por una característica que es enviada por el FRONT
 Route::post('/v1/filter/product', [ProductController::class, 'filterProduct']);
